1426dd7a7c03596e4cca762601ce2c8fbeb4b8d5
abhishek1337chatterjee <abhishek.c@latitudetechnolab.com>
2023-08-17 12:59:20 +0530

    started learning socket.io

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = expres();
+const path = require('path');
+
 
 const PORT = proces.env.PORT || 4000;
 
 const server = app.listen(PORT, () =>
   console.log(`📢 Server is listening ${PORT}`)
 );
+
+
+app.use(express.static(path.join(__dirname, 'public')))
