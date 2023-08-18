010bf28d09062b95c37af8399d31bd0b11d08ef3
abhishek1337chatterjee <abhishek.c@latitudetechnolab.com>
2023-08-18 12:11:04 +0530

    working on socket.io

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,34 @@ const app = express();
 const path = require('path');
 
 
+
 const PORT = process.env.PORT || 4000;
 
 const server = app.listen(PORT, () =>
   console.log(`📢 Server is listening ${PORT}`)
 );
+
+const io = require('socket.io')(server);
+
+// io.on('connection',(socket)=>{
+//   console.log(socket.id)
+// })
+
+let socketsConnected = new Set();
+
+io.on('connection', onConnected)
+ function onConnected(socket) {
+  console.log(socket.id);
+  socketsConnected.add(socket.id, socketsConnected.size);
+ 
+io.emit('clients-total')
+
+  socket.on('disconnect', () => {
+    console.log('socket disconnected', socket.id);
+    io.emit('clients-total', socketsConnected.size)
+    socketsConnected.delete(socket.id);
+  })
+ }
 
 
 app.use(express.static(path.join(__dirname, 'public')))
