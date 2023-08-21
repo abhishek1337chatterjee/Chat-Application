afb7f488ebf8cefcf9d79c561eeef59d57a18b8b
abhishek1337chatterjee <abhishek.c@latitudetechnolab.com>
2023-08-21 19:19:13 +0530

    done uptill receiving messages from both the user

--- Backend/public/main.js
+++ Backend/public/main.js
@@ -1,43 +1,51 @@
 const socket = io();
-const clientsTotal = document.getElementById('clients-total');
-const messageContainer = document.getElementById('message-container');
+const clientsTotal = document.getElementById("clients-total");
+const messageContainer = document.getElementById("message-container");
 const nameInput = document.getElementById("name-input");
 const messageForm = document.getElementById("message-form");
 const messageInput = document.getElementById("message-input");
 
-messageForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    sendMessage();
-})
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendMessage();
+});
 
-socket.on('clients-total', (data)=>{
-    clientsTotal.innerText = `Total Clients: ${data}`
+socket.on("clients-total", (data) => {
+  clientsTotal.innerText = `Total Clients: ${data}`;
+});
 
-})
-
-function sendMessage(){
-    console.log(messageInput.value)
-    const data = {
-        name: nameInput.value,
-        message: messageInput.value,
-        dateTime: new Date()
+function sendMessage() {
+    if(messageInput.value ===''){
+        return;
     }
-    socket.emit('message', data)
-    addMessageToUI(true, data);
-    messageInput.value = '';
+  console.log(messageInput.value);
+  const data = {
+    name: nameInput.value,
+    message: messageInput.value,
+    dateTime: new Date(),
+  };
+  socket.emit("message", data);
+  addMessageToUI(true, data);
+  messageInput.value = "";
 }
 
-socket.on('chat-message',(data) => {
-    // console.log(data);
-    addMessageToUI(false,data)
-})
+socket.on("chat-message", (data) => {
+  // console.log(data);
+  addMessageToUI(false, data);
+});
 
-function addMessageToUI(isOwnMessage,data){
-    const elment = `<li class="${isOwnMessage} ? "message-right" : "message-left">
+function addMessageToUI(isOwnMessage, data) {
+  const element = `
+    <li class="${isOwnMessage ? "message-right" : "message-left"}">
     <p class="message">
       ${data.message}
       <span>${data.name}🔯 ${moment(data.dateTime).fromNow()}</span>
     </p>
-  </li>`
-  messageContainer.innerHTML += elment
+  </li>`;
+  messageContainer.innerHTML += element;
+  scrollToBottom()
+}
+
+function scrollToBottom(){
+    messageContainer.scrollTo(0,messageContainer.scrollHeight)
 }
